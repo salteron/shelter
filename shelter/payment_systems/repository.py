2a721466bd580a2a3ff7654f84394d073e5ce690
Mikhail Sogomonyan <ms.sogomonyan@gmail.com>
2022-03-20 15:04:35 +0700

    f

--- shelter/payment_systems/repository.py
+++ shelter/payment_systems/repository.py
@@ -1,6 +1,6 @@
-import typing
+from typing import Dict, Type
 
 from shelter.payment_systems.models import PaymentSystem
 from shelter.payment_systems.superpay import Superpay
 
-PAYMENT_SYSTEM_BY_ID: typing.Dict[str, PaymentSystem] = {"superpay": Superpay}
+PAYMENT_SYSTEM_BY_ID: Dict[str, Type[PaymentSystem]] = {"superpay": Superpay}
