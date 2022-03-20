9f3c5e861f8aebb99ff32d420450b4875693da67
Mikhail Sogomonyan <ms.sogomonyan@gmail.com>
2022-03-20 10:53:10 +0700

    а

--- shelter/deposits/tasks.py
+++ shelter/deposits/tasks.py
@@ -2,6 +2,12 @@ from shelter.deposits import services
 
 HTTP_RETRY_EXCEPTIONS = (TimeoutError,)  # etc
 
+"""
+
+    Конечная реализация может использовать Celery для реализации очередей задач.
+
+"""
+
 
 def create_payment_system_deposit_task(deposit_id):
     services.create_payment_system_deposit(deposit_id)
@@ -12,13 +18,20 @@ create_payment_system_deposit_task.autoretry_for = HTTP_RETRY_EXCEPTIONS
 create_payment_system_deposit_task.max_retries = 5
 
 
-# возможно в джобе можно отметить, что джоб будет пытаться выполниться ограниченно число раз
-# а затем фоновая штука пройдется по старым Payout в статусе PENDING (!) и вернет деньги на базу,
-# предварительно переведя их в статус canceled.
 def create_payment_system_payout_task(payout_id):
+    """
+
+    Для простоты предполагаем, что в случае ошибок задача переставляется бесчисленное
+    количество раз до тех пор, пока не выполнится.
+
+    В конечной реализации число повторных попыток должно быть ограничено, а Celery Beat
+    регулярно запускает задачу, которая отыскивает payouts в статусе CREATED,
+    созданные более t назад, и релизит соответствующие ("висящие") холды.
+
+    """
     services.create_payment_system_payout(payout_id)
 
 
 create_payment_system_payout_task.delay = lambda payout_id: None
 create_payment_system_payout_task.autoretry_for = HTTP_RETRY_EXCEPTIONS
-create_payment_system_payout_task.max_retries = 5
+create_payment_system_payout_task.max_retries = None  # infinity
